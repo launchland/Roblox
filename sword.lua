45ba19986d52a662ca1ec2b3908014f3ee495bbc
launchland <benjulian.noel@gmail.com>
2024-09-24 22:08:24 -0600

    Update sword.lua

--- sword.lua
+++ sword.lua
@@ -1,4 +1,5 @@
 -- NOTE: this script sits under a tool in StarterPack
+-- Can be seen in use: https://www.roblox.com/games/5727187297/
 local tool = script.Parent
 	local function whenTouched(otherPart)
 	local otherHumanoid = otherPart.Parent:FindFirstChild("Humanoid")
